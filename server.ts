1664d8cd47b54b3c4139ca2212ddb121850c25a7
DarNCM <216679882+DarNCM@users.noreply.github.com>
2025-08-26 10:32:48 +0200

    Put in other Exercises

--- server.ts
+++ server.ts
@@ -1,70 +1,35 @@
-// Imports express out of node_modules, and fs/path from node directly
-import express from "express";
-import fs from "fs";
-import path from "path";
 
-// declaring variable for the server (app = express) and for the Port we will be using
-const app = express();
-const PORT = 3000;
-// app.use(...) lets the user use the public site, and express.json lets the server understand json-data from formulars (like the registry-formular that we want to save as json)
-app.use(express.static("public"));
-app.use(express.json());
-// const dataPath saves the path to the json file as a variable 
-// path.join connects the path so it works on every operating system
-const dataPath = path.join(import.meta.dirname, "users.json");
-// app.post defines a Route that only reacts on /register
-// req: Request is an Object that gathers Information from the request made by app.post("/register")
-// res: Response - responds after gathering the data from req.body
-app.post("/register", (req, res) => {
-//here we would get the full rey.body = the full data from the page but we extract the data with {...} and express what will happen via Arrow Function
-const { username, password, email } = req.body;
 
-//We ask the following: if No Username, OR no Password OR no email has been put into either field, we would get an error (400)
-//if done correctly, they would get the Message from (200)
-if (!username || !password || !email) {
-  res.status(400).json({ error: "Alle Felder sind erforderlich!" }).send();
+function sumOfTrippledEvens(array: number[]): number {
+  return array
+    .filter((num: number) => num % 2 === 0)
+    .map((num: number) => num * 3)
+    .reduce((sum: number, num: number) => sum + num, 0);
 }
-else {
-  res.status(200).json({ message: "Erfolgreiche Eingabe!" }).send();
+
+console.log(sumOfTrippledEvens([1, 2, 3, 4, 5]));
+
+
+function camelize(str: string): string {
+  return str
+  .split('-')
+  .map(                                   // "?" is a ternary operator (if true, return word, if false, return word[0].toUpperCase() + word.slice(1))
+    (word: string, index: number) => index == 0 ? word : word[0].toUpperCase() + word.slice
+    (1)
+  )
+  .join('');
 }
-  const users = req.body;
-  fs.writeFileSync(dataPath, JSON.stringify(users, null, 2), "utf-8"); 
 
-const newUser = {username, password, email};
+// "?" wenn das stimmt (true) dann wird das erste wort ausgegeben, 
+// wenn das nicht stimmt (false) dann wird das zweite wort ausgegeben, also das
+// was nach dem ":" steht
 
-// 1. Load existing users from file (or start with an empty array)
-let nUsers = [];
-if (fs.existsSync(dataPath)) {
-  const fileData = fs.readFileSync(dataPath, "utf-8");
-  try {
-    nUsers = JSON.parse(fileData);
-    if (!Array.isArray(users)) {
-      nUsers = []; // If somehow it's not an array
-    }
-  } catch (err) {
-    console.error("Fehler beim Parsen von users.json:", err);
-    nUsers = []; // On JSON error, start fresh
-  }
+
+function filterRange (arr, a, b){
+  return arr.filter(item => (a <= item && item <= b));
 }
-  // 2. Add (push) new user to the array
-nUsers.push(newUser);
-
-// 3. Save the updated user list back to the file
-fs.writeFileSync(dataPath, JSON.stringify(nUsers, null, 2), "utf-8");
-
-});
-
-
-
-
-
-
-
-  app.listen(PORT, () => {
-  console.log(`Server läuft auf http://localhost:${PORT}`);
-});
-
-
-
-
-
+//diese funktion filtert die werte zwischen a und b aus dem array arr aus
+// "item" ist der wert des arrays, da kann man auch "num" schreiben oder sonst was
+// "a <= item && item <= b" ist die bedingung, die erfüllt sein muss, damit der wert in das array kommt
+// "a" ist der kleinste wert, der in das array kommen kann
+// "b" ist der größte wert, der in das array kommen kann
